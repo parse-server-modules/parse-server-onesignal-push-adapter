99ceb5f40c904d3f38e5456ad87ac6cd5518d6f1
Rob <robtheriault@gmail.com>
2016-04-29 21:52:45 -0400

    Revert "Trying again at spec file."
    This reverts commit 799e1bf7094e98201e2e7238e65b63c933e35e0b.

--- spec/OneSignalPushAdapter.spec.js
+++ spec/OneSignalPushAdapter.spec.js
@@ -179,7 +179,7 @@ describe('OneSignalPushAdapter', () => {
 
     oneSignalPushAdapter.sendToGCM(backgroundPush,[{'deviceToken':'androidToken1'},{'deviceToken':'androidToken2'}])
     expect(sendToOneSignal).toHaveBeenCalled();
-    var args = sendToOneSignal.calls.argsFor(0).args;
+    var args = sendToOneSignal.calls.first().args;
     expect(args[0]).toEqual({
       'android_background_data': true,
       'data':{'misc-data':'Example Data'},
@@ -188,7 +188,7 @@ describe('OneSignalPushAdapter', () => {
 
     oneSignalPushAdapter.sendToAPNS(backgroundPush,[{'deviceToken':'iosToken1'},{'deviceToken':'iosToken2'}])
     expect(sendToOneSignal).toHaveBeenCalled();
-    args = sendToOneSignal.calls.argsFor(1).args;
+    var args = sendToOneSignal.calls.first().args;
     expect(args[0]).toEqual({
       'content_available': true,
       'data':{'misc-data':'Example Data'},
